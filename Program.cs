016ee7a20e0ac300357f6c8f0358564e81b74412
xuweicheng <weicheng.xu@gmail.com>
2018-03-26 14:06:56 -0700

    initial checkin

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.WindowsServices;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace CoreApiHostedAsWindowsService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var exePath = Process.GetCurrentProcess().MainModule.FileName;
+            var contentRootPath = Path.GetDirectoryName(exePath);
+
+            var host = WebHost.CreateDefaultBuilder(args)
+                .UseContentRoot(contentRootPath)
+                .UseStartup<Startup>()
+                .Build();
+
+            host.RunAsService();
+
+            //BuildWebHost(args).Run();
+        }
+
+        //public static IWebHost BuildWebHost(string[] args) =>
+        //    WebHost.CreateDefaultBuilder(args)
+        //        .UseStartup<Startup>()
+        //        .Build();
+    }
+}
